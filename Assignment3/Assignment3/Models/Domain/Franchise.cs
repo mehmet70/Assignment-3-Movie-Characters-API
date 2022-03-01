6bacbe68119b0208f6418123ac65d6c2fec7ff5c
Tiemar Mepschen <TiemarMepschen@gmail.com>
2022-03-01 15:24:10 +0100

    feat: Added franchise class

---
+++ Assignment3/Assignment3/Models/Domain/Franchise.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment3.Models.Domain
+{
+    public class Franchise
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(1000)]
+        public string Description { get; set; }
+    }
+}
